1820233366bd4c5dab6bc61810bb16e767832160
alexzwir <alexzwir@gmail.com>
2018-10-15 21:48:06 -0300

    Adding the Our Work template more the configure of URLs and the view function that handle it.

--- website/urls.py
+++ website/urls.py
@@ -20,6 +20,7 @@ from website.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('nossostrabalhos.html', views.work,name="work"),
     
 ]
