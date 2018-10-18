03d453c611b74e7f29f0b1cb4e3ca76121bdf647
alexzwir <alexzwir@gmail.com>
2018-10-18 09:02:06 -0300

    Addint the Message field on the Models and ajusting the contact page

--- website/core/models.py
+++ website/core/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
+from django import forms
+
 
 # Create your models here.
 
 class ContactModel(models.Model):
-    name_contact = models.CharField(max_length=50)
-    email_contact = models.CharField(max_length=30)
-    phone_contact = models.CharField(max_length=30)
+    contact_name = models.CharField(max_length=50)
+    contact_email = models.CharField(widget=forms.EmailInput, max_length=30)
+    contact_phone = models.CharField(max_length=30)
     contact_sent_date = models.CharField(max_length=35,default=timezone.now)
+    contact_message = models.CharField(max_length=50)
 
     class Meta:
         verbose_name = "Contato"
         verbose_name_plural = "Contatos"
 
     def __str__(self):
-        return smart_text(self.name_contact)
+        return smart_text("{} | {}".format(self.contact_name,self.contact_email))
 
