1f128a9138171f0f66edb9b540d5d8008ec2c7c9
alexzwir <alexzwir@gmail.com>
2018-10-20 17:42:41 -0300

    Adding DateTimeField, EmailField and TextField on the ContactModel

--- website/core/models.py
+++ website/core/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
-from django import forms
-
-
-# Create your models here.
 
 class ContactModel(models.Model):
     contact_name = models.CharField(max_length=50)
-    contact_email = models.CharField(max_length=30)
+    contact_email = models.EmailField(max_length=30)
     contact_phone = models.CharField(max_length=30)
-    contact_sent_date = models.CharField(max_length=35,default=timezone.now)
-    contact_message = models.CharField(max_length=50)
+    contact_sent_date = models.DateTimeField(max_length=35,default=timezone.now)
+    contact_message = models.TextField(max_length=500)
 
     class Meta:
         verbose_name = "Contato"
