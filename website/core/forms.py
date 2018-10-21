f864b3121866c7d9f07ef7f33a3c99895403da5f
alexzwir <alexzwir@gmail.com>
2018-10-21 18:24:37 -0300

    Connecting the form in the Contact page with the ContactForm

--- website/core/forms.py
+++ website/core/forms.py
@@ -3,7 +3,13 @@ from django.utils import timezone
 
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(max_length=50,label="Nome")
-    contact_email = forms.EmailField(max_length=30,label="Email")
-    contact_phone = forms.CharField(max_length=30,label="Telefone")
-    contact_message = forms.CharField(max_length=500,label="Mensagem")
+    contact_name = forms.CharField(max_length=50,label="",widget=forms.TextInput(attrs={'placeholder': 'Nome','size': '50',"class":"form-control"}))
+    contact_email = forms.EmailField(max_length=30,label="",widget=forms.TextInput(attrs={'placeholder': 'Email','size': '50',"class":"form-control"}))
+    contact_phone = forms.CharField(max_length=30,label="",widget=forms.TextInput(attrs={'placeholder': 'Telefone','size': '50',"class":"form-control"}))
+    contact_message = forms.CharField(max_length=500,label="",widget=forms.Textarea(attrs={'placeholder':'Deixe sua mensagem aqui',"class": "form-control"}))
+
+
+"""
+Took out the label and insert the placeholder for forms
+contact_phone = forms.CharField(max_length=30,label="Telefone")
+"""
